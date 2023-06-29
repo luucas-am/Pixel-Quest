84b2e8830ea911ad2d4d7c578c4a0538a0d76513
Lucas Andrade <lucas.andrade7899@gmail.com>
2023-06-29 17:13:37 -0300

    Trampoline, Bouncy Physics

---
+++ Assets/Scripts/TrampolineAnimationHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineAnimationHandler : MonoBehaviour
+{
+    [SerializeField] Animator trampolineAnimator;
+    [SerializeField] CapsuleCollider2D trampolineCollider;
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (trampolineCollider.IsTouchingLayers(LayerMask.GetMask("Default")))
+            trampolineAnimator.SetTrigger("isWorking");
+    
+    }
+}
