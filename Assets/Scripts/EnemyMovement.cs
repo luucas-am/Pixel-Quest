1b3c5fc520bb3f79ce6191e7e763cd7d81295497
Lucas Andrade <lucas.andrade7899@gmail.com>
2023-07-03 20:42:36 -0300

    Kabuu Running/Idling Animation/Logic

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,20 +5,67 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Rigidbody2D kabuuRigidbody;
+    Rigidbody2D enemyRigidbody;
+    Animator enemyAnimator;
 
+    bool isWalking = true;
+    float stopChance = 0.2f; // Adjust this value to control the likelihood of stopping
+    float stopDuration = 1f; // Adjust this value to set the duration of the stop
+    float timer = 0f;
     [SerializeField] float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        kabuuRigidbody = GetComponent<Rigidbody2D>();
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+        enemyAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        kabuuRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        Move();
+    }
+
+    void Move()
+    {
+        if (isWalking)
+        {
+            enemyRigidbody.velocity = new Vector2(moveSpeed, 0f);
+            timer += Time.deltaTime;
+
+            if (timer >= stopDuration)
+            {
+                float randomValue = UnityEngine.Random.value;
+
+                if (randomValue <= stopChance)
+                {
+                    // Stop the enemy
+                    enemyRigidbody.velocity = Vector2.zero;
+                    isWalking = false;
+                    timer = 0f;
+                }
+                else
+                {
+                    // Continue walking
+                    timer = 0f;
+                }
+            }
+        }
+        
+        else
+        {
+            // Enemy is currently stopped
+            timer += Time.deltaTime;
+
+            if (timer >= stopDuration)
+            {
+                // Resume walking
+                isWalking = true;
+                timer = 0f;
+            }
+        }
+        enemyAnimator.SetBool("isRunning", isWalking);
     }
 
     void OnTriggerExit2D(Collider2D other) 
@@ -29,6 +76,6 @@ public class EnemyMovement : MonoBehaviour
 
     void FlipSprite()
     {
-        transform.localScale = new Vector2 (-(Mathf.Sign(kabuuRigidbody.velocity.x)), 1f);
+        transform.localScale = new Vector2 (-(Mathf.Sign(enemyRigidbody.velocity.x)), 1f);
     }
 }
